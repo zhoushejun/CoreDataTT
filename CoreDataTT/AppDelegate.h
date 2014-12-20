98328f7de514db85871b35308df73f98026ad968
周社军 <965678322@qq.com>
2014-12-20 22:55:45 +0800

    Initial Commit

---
+++ CoreDataTT/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataTT
+//
+//  Created by zhoushejun on 14-12-20.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CoreDataTT/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataTT
+//
+//  Created by zhoushejun on 14-12-20.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
